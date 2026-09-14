fc7bded1d31ddcbe935ba18e0948d4512084c05a
Emilio R <125053857@upq.edu.mx>
2026-09-14 17:19:41 -0600

    Se trabajó con arreglos unidimensionales

---
+++ Arreglos.py
@@ -0,0 +1,31 @@
+#Declarando un arreglo
+numeros=[10,20,30,40,50]
+
+#Imprime un elemento específico que se encuentre dentro del arreglo
+print(numeros[2])
+
+#Reasignación del valor de una posición
+numeros[3]=35
+
+#Imprimimos todo el arreglo
+print(numeros)
+
+#Agregar nuevo valor al final del arreglo
+numeros.append(60)
+print(numeros)
+
+#Removemos un valor dentro del arreglo
+numeros.remove(35)
+print(numeros)
+
+#Eliminamos un valor del arreglo utilizando la posición
+numeros.pop(4)
+print(numeros)
+
+frutas=["manzana", "fresa", "sandia", "mango", "melon", "platano"]
+frutas.pop(4)
+print(frutas)
+
+frutas.remove("manzana")
+print(frutas)
+
